bf690978e6a287c9d8b635ee9a2b5cebcb062464
opsingh861 <62853140+opsingh861@users.noreply.github.com>
2023-07-17 13:04:41 +0530

    Task management application completed

--- scripts/index.js
+++ scripts/index.js
@@ -11,10 +11,10 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
 <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
 <div class ='card shadow-sm task__card'>
 <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
-<button type='button' class='btn btn-outline-info mr-2' name=${id}>
+<button type='button' class='btn btn-outline-info mr-2' name=${id} onclick="editTask.apply(this,arguments)">
 <i class="fas fa-pencil-alt" name=${id}></i>
 </button>
-<button type="button" class="btn btn-outline-danger mr-2" name=${id}>
+<button type="button" class="btn btn-outline-danger mr-2" name=${id} onclick="deleteTask.apply(this,arguments)">
 <i class="fas fa-trash-alt" name=${id}></i>
 </button>
 </div>
@@ -46,8 +46,8 @@ const htmlModalContent = ({ id, title, description, url }) => {
     return `
     <div id=${id}>
     ${url ?
-            `<img width=100% src=${url} alt="card image cap" class='img-fluid place__holder__image mb-3'/>`
-            : `<img width='100%' src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png" alt="card image cap" class="img-fluid place__holder__image mb-3" />`
+            `<img width=100% height="100px" style="object-fit:cover; object-position:center;" src=${url} alt="card image cap" class='img-fluid place__holder__image mb-3'/>`
+            : `<img width='100%' height="100px" style="object-fit:cover; object-position:center;"  src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png" alt="card image cap" class="img-fluid place__holder__image mb-3" />`
         }
     </div>
     <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>
@@ -105,4 +105,131 @@ const openTask = (e) => {
     if (!e) e = window.event;
     const getTask = state.taskList.find(({ id }) => id === e.target.id)
     taskModal.innerHTML = htmlModalContent(getTask)
+}
+
+const deleteTask = (e) => {
+    // console.log(e.target)
+    if (!e) e = window.event;
+    const targetID = e.target.getAttribute("name")
+    const type = e.target.getName;
+    const removeTask = state.taskList.filter(({ id }) => id !== targetID)
+
+    state.taskList = removeTask
+    updateLocalStorage()
+
+    if (type === "BUTTON") {
+        // console.log(e.target.parentNode.parentNode.parentNode.parentNode)
+        return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+            e.target.parentNode.parentNode.parentNode
+        )
+    }
+
+    return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+        e.target.parentNode.parentNode.parentNode.parentNode
+    )
+}
+
+
+
+const editTask = (e) => {
+    if (!e) e = window.event;
+
+    const targetID = e.target.id;
+    const type = e.target.tagName;
+
+    let parentNode;
+    let taskTitle;
+    let taskDescription;
+    let taskType;
+    let submitButton;
+
+    if (type === "BUTTON") {
+        parentNode = e.target.parentNode.parentNode;
+    }
+    else {
+        parentNode = e.target.parentNode.parentNode.parentNode
+    }
+
+    taskTitle = parentNode.childNodes;
+    console.log(taskTitle)
+
+
+    taskTitle = parentNode.childNodes[3].childNodes[3];
+    taskDescription = parentNode.childNodes[3].childNodes[5];
+    taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    submitButton = parentNode.childNodes[5].childNodes[1];
+
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+
+    submitButton.setAttribute("onclick", "saveEdit.apply(this,arguments)");
+    submitButton.removeAttribute("data-bs-toggle")
+    submitButton.removeAttribute("data-bs-target")
+    submitButton.innerHTML = "Save Changes"
+}
+
+
+const saveEdit = (e) => {
+    if (!e) e = window.event;
+
+    const targetID = e.target.id;
+    const parentNode = e.target.parentNode.parentNode;
+
+    const taskTitle = parentNode.childNodes[3].childNodes[3];
+    const taskDescription = parentNode.childNodes[3].childNodes[5];
+    const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    const submitButton = parentNode.childNodes[5].childNodes[1];
+
+
+    const updateData = {
+        taskTitle: taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML
+    }
+
+    let stateCopy = state.taskList;
+
+
+    stateCopy = stateCopy.map((task) =>
+        task.id === targetID ? {
+            id: task.id,
+            title: updateData.taskTitle,
+            description: updateData.taskDescription,
+            url: task.url
+        } : task
+    )
+
+    state.taskList = stateCopy
+    updateLocalStorage()
+
+    taskTitle.setAttribute("contenteditable", "false");
+    taskDescription.setAttribute("contenteditable", "false");
+    taskType.setAttribute("contenteditable", "false");
+
+
+    submitButton.setAttribute("onclick", "openTask.apply(this,arguments)")
+    submitButton.setAttribute("data-bs-toggle", "modal")
+    submitButton.setAttribute("data-bs-target", "#showTask")
+    submitButton.innerHTML = "Open Task"
+}
+
+
+const searchTask = (e) => {
+    if (!e) e = window.event;
+
+
+    while (taskContents.firstChild) {
+        taskContents.removeChild(taskContents.firstChild)
+    }
+
+    const resultData = state.taskList.filter(({ title }) => {
+        return title.toLowerCase().includes(e.target.value.toLowerCase());
+    })
+    // console.log(resultData)
+
+    resultData.map((cardData) => {
+        taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardData))
+    })
 }
